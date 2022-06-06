40cddec1d2cecea64bff2f00796900933fac3fc7
jp-rad <jp-rad@example.com>
2022-06-06 16:13:02 +0000

    My Parcel App

---
+++ src/https.ts
@@ -0,0 +1,10 @@
+import MyLogger from './api/mylogger';
+
+function doGet(e: GoogleAppsScript.Events.DoGet) {
+    MyLogger.log(JSON.stringify(e));
+    const template = HtmlService.createTemplateFromFile("index");
+    template.querystring = JSON.stringify(e);
+    return template
+            .evaluate()
+            .addMetaTag("viewport", "width=device-width, initial-scale=1.0");
+}
